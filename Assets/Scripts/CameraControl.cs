241e0b102b8eb37ae3ad225296d37f3f1995bb50
trongr <devgruxcon@gmail.com>
2018-03-19 01:29:13 -0400

    Player movement and pointing

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	private Transform player;
+	private Vector3 offset;
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;	
+		offset = transform.position - player.transform.position;
+	}
+	
+	void Update () {
+		Vector3 target = player.position + offset;
+		transform.position = Vector3.Lerp (transform.position, target, Time.deltaTime * 8);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerControl : MonoBehaviour {
+
+	public float rotation_speed = 1f;
+	public float walk_speed = 5f;
+	public float run_speed = 8f;
+
+	private Quaternion target_rotation;
+	private CharacterController control;
+
+	void Start () {
+		control = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		ControlMouse ();
+	}
+
+	void move_player (Vector3 input) {
+		Vector3 motion = input;
+		motion *= (Mathf.Abs (input.x)== 1 && Mathf.Abs (input.z)== 1?.707f : 1f);
+		motion *= (Input.GetButton ("Run")? run_speed : walk_speed);
+		motion += Vector3.up * -8;
+		control.Move (motion * Time.deltaTime);
+	}
+
+	void ControlMouse () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+
+		Plane playerPlane = new Plane (Vector3.up, transform.position);
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		float hitdist = 0.0f;
+		if (playerPlane.Raycast (ray, out hitdist)) {
+			Vector3 targetPoint = ray.GetPoint (hitdist);
+			Quaternion targetRotation = Quaternion.LookRotation (targetPoint - transform.position);
+			transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
+		}
+
+		move_player (input);
+	}
+
+	void ControlWASD () {
+		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+
+		if (input != Vector3.zero) {
+			target_rotation = Quaternion.LookRotation (input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, target_rotation.eulerAngles.y, rotation_speed * Time.deltaTime);
+		}
+
+		move_player (input);
+	}
+}
