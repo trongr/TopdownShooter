b9b66bd68c039c8cb813349c33af64c14e40bb19
trongr <devgruxcon@gmail.com>
2018-03-25 02:31:08 -0400

    Shooting + semi vs auto

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	public Transform spawn;
+	public enum GunType { Semi, Burst, Auto }
+	public GunType gun_type;
+
+	public void Shoot() {
+		Ray ray = new Ray(spawn.position, spawn.forward);
+		RaycastHit hit;
+		float shot_distance = 20;
+		if (Physics.Raycast(ray, out hit, shot_distance)) {
+			shot_distance = hit.distance;
+		}
+		Debug.DrawRay(ray.origin, ray.direction * shot_distance, Color.red, 1);
+	}
+
+	public void ShootContinuous() {
+		if (gun_type == GunType.Auto) {
+			Shoot();
+		} // TODO shoot burst
+	}
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,55 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof (CharacterController))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour {
 
 	public float rotation_speed = 1f;
 	public float walk_speed = 5f;
 	public float run_speed = 8f;
 
+	public Gun gun;
+
 	private Quaternion target_rotation;
 	private CharacterController control;
 
-	void Start () {
-		control = GetComponent<CharacterController> ();
+	void Start() {
+		control = GetComponent<CharacterController>();
 	}
 
-	void Update () {
-		ControlMouse ();
+	void Update() {
+		// ControlWASD();
+		ControlMouse();
+		if (Input.GetButtonDown("Shoot")) {
+			gun.Shoot();
+		} else if (Input.GetButton("Shoot")) {
+			gun.ShootContinuous();
+		}
 	}
 
-	void move_player (Vector3 input) {
+	void move_player(Vector3 input) {
 		Vector3 motion = input;
-		motion *= (Mathf.Abs (input.x)== 1 && Mathf.Abs (input.z)== 1?.707f : 1f);
-		motion *= (Input.GetButton ("Run")? run_speed : walk_speed);
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1 ? 0.707f : 1f);
+		motion *= (Input.GetButton("Run") ? run_speed : walk_speed);
 		motion += Vector3.up * -8;
-		control.Move (motion * Time.deltaTime);
+		control.Move(motion * Time.deltaTime);
 	}
 
-	void ControlMouse () {
-		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+	void ControlMouse() {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-		Plane playerPlane = new Plane (Vector3.up, transform.position);
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Plane playerPlane = new Plane(Vector3.up, transform.position);
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		float hitdist = 0.0f;
-		if (playerPlane.Raycast (ray, out hitdist)) {
-			Vector3 targetPoint = ray.GetPoint (hitdist);
-			Quaternion targetRotation = Quaternion.LookRotation (targetPoint - transform.position);
-			transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
+		if (playerPlane.Raycast(ray, out hitdist)) {
+			Vector3 targetPoint = ray.GetPoint(hitdist);
+			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
 		}
 
-		move_player (input);
+		move_player(input);
 	}
 
-	void ControlWASD () {
-		Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.GetAxisRaw ("Vertical"));
+	void ControlWASD() {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
 		if (input != Vector3.zero) {
-			target_rotation = Quaternion.LookRotation (input);
-			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle (transform.eulerAngles.y, target_rotation.eulerAngles.y, rotation_speed * Time.deltaTime);
+			target_rotation = Quaternion.LookRotation(input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, target_rotation.eulerAngles.y, rotation_speed * Time.deltaTime);
 		}
 
-		move_player (input);
+		move_player(input);
 	}
 }
