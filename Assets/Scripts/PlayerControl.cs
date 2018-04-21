dda88e355667a904b5f68750bdacb8a30297acff
trong <devgruxcon@gmail.com>
2018-04-21 19:44:56 -0400

    Player acceleration option

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerControl : MonoBehaviour {
 
-	public float rotation_speed = 1f;
+	public float rotation_speed = 10f;
 	public float walk_speed = 5f;
 	public float run_speed = 8f;
+	public float acceleration = 5f; // This doesn't seem to do anything.
 
 	public Gun gun;
 
 	private Quaternion target_rotation;
+	private Vector3 currentVelocityMod;
 	private CharacterController control;
 
 	void Start() {
@@ -29,6 +31,8 @@ public class PlayerControl : MonoBehaviour {
 	}
 
 	void move_player(Vector3 input) {
+		// currentVelocityMod = Vector3.MoveTowards(currentVelocityMod, input, acceleration * Time.deltaTime);
+		// Vector3 motion = currentVelocityMod;
 		Vector3 motion = input;
 		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1 ? 0.707f : 1f);
 		motion *= (Input.GetButton("Run") ? run_speed : walk_speed);
@@ -38,7 +42,6 @@ public class PlayerControl : MonoBehaviour {
 
 	void ControlMouse() {
 		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
 		Plane playerPlane = new Plane(Vector3.up, transform.position);
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		float hitdist = 0.0f;
@@ -47,18 +50,15 @@ public class PlayerControl : MonoBehaviour {
 			Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
 			transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotation_speed * Time.deltaTime);
 		}
-
 		move_player(input);
 	}
 
 	void ControlWASD() {
 		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-
 		if (input != Vector3.zero) {
 			target_rotation = Quaternion.LookRotation(input);
 			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, target_rotation.eulerAngles.y, rotation_speed * Time.deltaTime);
 		}
-
 		move_player(input);
 	}
 }
