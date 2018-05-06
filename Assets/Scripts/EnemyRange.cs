6c133f0b4b09ac4075b55c593dd5ed7719fcb275
trong <devgruxcon@gmail.com>
2018-05-06 18:22:39 -0400

    Follow player

---
+++ Assets/Scripts/EnemyRange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour {
+
+	public float followPlayerSpeed = 1f;
+	private Transform playerTransform;
+
+	void Update() {
+		FollowPlayer();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			playerTransform = other.transform;
+		}
+	}
+
+	private void FollowPlayer() {
+		if (playerTransform != null) {
+			transform.position = Vector3.MoveTowards(transform.position, playerTransform.position, followPlayerSpeed * Time.deltaTime);
+		}
+	}
+
+}
